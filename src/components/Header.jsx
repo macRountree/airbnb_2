f5e4fce37371a1d3c36de9978e422b7b05943da7
Mac Rountree <a219206225@unison.mx>
2023-11-12 10:48:08 -0700

    Add date range picker and guest selection to Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,25 @@
 'use client';
 import Image from 'next/image';
-
 import { useState } from 'react';
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import { DateRangePicker } from 'react-date-range';
 
 const Header = () => {
   const [searchInput, setSearchInput] = useState('');
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+  const [numOfGuests, setNumOfGuest] = useState(1);
+  const handleSelect = ranges => {
+    setStartDate(ranges.selection.startDate);
+    setEndDate(ranges.selection.endDate);
+    console.log(ranges);
+  };
+  const selectionRange = {
+    startDate,
+    endDate,
+    key: 'selection',
+  };
   return (
     <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
       <div className="relative flex items-center h-10 cursor-pointer my-auto">
@@ -13,6 +28,7 @@ const Header = () => {
           layout="fill"
           objectFit="contain"
           objectPosition="left"
+          alt="MIbnb"
         />
       </div>
       <div
@@ -82,7 +98,45 @@ const Header = () => {
           </svg>
         </div>
       </div>
-      {searchInput && <div>Holla Fucker</div>}
+      {searchInput && (
+        <div className="flex flex-col col-span-3 mx-auto mt-5">
+          {' '}
+          <DateRangePicker
+            ranges={[selectionRange]}
+            minDate={new Date()}
+            rangeColors={['#FD5861']}
+            onChange={handleSelect}
+          />{' '}
+          <div className="flex items-center border-b mb-4">
+            <h2 className="text-2xl pl-2 flex-grow font-semibold ">
+              Number of Guest
+            </h2>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="w-5 h-5"
+            >
+              <path d="M4.5 6.375a4.125 4.125 0 118.25 0 4.125 4.125 0 01-8.25 0zM14.25 8.625a3.375 3.375 0 116.75 0 3.375 3.375 0 01-6.75 0zM1.5 19.125a7.125 7.125 0 0114.25 0v.003l-.001.119a.75.75 0 01-.363.63 13.067 13.067 0 01-6.761 1.873c-2.472 0-4.786-.684-6.76-1.873a.75.75 0 01-.364-.63l-.001-.122zM17.25 19.128l-.001.144a2.25 2.25 0 01-.233.96 10.088 10.088 0 005.06-1.01.75.75 0 00.42-.643 4.875 4.875 0 00-6.957-4.611 8.586 8.586 0 011.71 5.157v.003z" />
+            </svg>
+            <input
+              value={numOfGuests}
+              onChange={e => setNumOfGuest(e.target.value)}
+              min={1}
+              className="w-12 pl-2 text-lg outline-none text-red-400"
+              type="number"
+            />
+          </div>
+          <div className="flex ">
+            <button className="flex-grow text-gray-600 cursor-pointer hover:bg-gray-600 hover:text-white  rounded-xl hover:scale-105 transition transform duration-200 ease-out">
+              Cancel
+            </button>
+            <button className="flex-grow text-red-400 hover:bg-red-400 hover:text-white cursor-pointer rounded-lg hover:scale-105 transition transform duration-200 ease-out">
+              Search
+            </button>
+          </div>
+        </div>
+      )}
     </header>
   );
 };
